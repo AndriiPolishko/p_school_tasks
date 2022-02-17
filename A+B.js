ab6299975b1b960eecb6eca0140adcf968442e23
Andrii <ramp8a799@gmail.com>
2022-02-17 15:31:04 +0200

    now it works with grater positive and lesser negative

--- A+B.js
+++ A+B.js
@@ -12,20 +12,69 @@ const equalizer = function(A,B) {
     return [A,B]
 }
 
-const a_plus_B = function(A,B) {
+const deEqualizer = function (res) {
+    let newRes ;
+    for(let i = 0; i < res.length; i++) {
+        if(res[i] !== "0") {
+            newRes = res.slice (i);
+            break
+        }
+    }
+    return newRes;
+}
 
+const a_plus_B = function(A,B) {
     A = A.replace(/,/g, '');
     B = B.replace(/,/g, '');
+
+    let aNeg = false, bNeg = false;
+    if(A[0] === "-") {
+        aNeg = true;
+        A = A.slice(1);
+    }
+    if(B[0] === "-") {
+        bNeg = true;
+        B = B.slice(1);
+    }
+
     [A,B] = equalizer(A,B)
+    let aGrater = false, bGrater = false;
+    if(A>B) aGrater = true;
+    else if (B>A)   bGrater = true;
+
     let res = "", temp;
     let n = A.length ;
     let buffer;
     for(let i = n-1; i >= 0; i--) {
             if(buffer) {
-                temp = (+A[i] + (+B[i]) +buffer).toString()
-                buffer = undefined
+                if(aNeg && bNeg || !aNeg && !bNeg) {
+                    temp = (+A[i] + +B[i] +buffer).toString()
+                    if(temp.length >1) {
+                        buffer = +temp[0];
+                        temp = temp[1]
+                    }else
+                        buffer = undefined
+                }
+                if(aNeg && !bNeg || !aNeg && bNeg) {
+                    temp = +A[i] - B[i] - 1
+                    if(temp < 0) {
+                        temp = 10 + temp
+                        buffer = -1;
+                    }
+                    else
+                        buffer = undefined
+                }
             } else {
-                    temp = (+A[i] + (+B[i])).toString()
+                if(aNeg && bNeg || !aNeg && !bNeg)
+                    temp = (+A[i] + +B[i]).toString()
+                else if(aNeg && !bNeg || !aNeg && bNeg) {
+                    temp = +A[i] - +B[i]
+                    if(temp < 0) {
+                        temp = 10 + temp
+                        buffer = -1;
+                    }
+                }
+
             }
             if(temp.length >1) {
                 buffer = +temp[0];
@@ -33,9 +82,11 @@ const a_plus_B = function(A,B) {
             }
             res = temp + res;
         }
-
+    res = deEqualizer(res)
+    if((aNeg && bNeg) || (aNeg && aGrater) || (bNeg && bGrater))
+        res = "-" + res;
     return res;
 }
 
-console.log(a_plus_B("12","123,456,780"))
+console.log(a_plus_B("1236","-89"))
 
